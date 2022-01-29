068ef3867dab8d7b494654d46668e7ec70241f7e
jungai <juworaphol125@gmail.com>
2022-01-29 17:40:48 +0700

    add bio component

---
+++ src/components/Bio.jsx
@@ -0,0 +1,24 @@
+import { tw } from "twind";
+
+const Bio = (props) => (
+  <div>
+    <h1 className={tw`uppercase text-4xl font-bold`}>
+      {props.firstName || "firstName"}
+      <br />
+      {props.lastName || "lastName"}
+    </h1>
+    <span className={tw`capitalize text-xl italic`}>
+      {props.currentRole || "role"}
+    </span>
+    <ul className={tw`mt-4`}>
+      {(props.contacts || []).map((contact) => (
+        <li className={tw`flex items-center space-x-2`} key={contact.text}>
+          {contact.icon}
+          <span>{contact.text}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default Bio;
