b3273098359ae1100afc462011283b01d0c7d305
jungai <juworaphol125@gmail.com>
2022-01-29 17:41:11 +0700

    add career objective component

---
+++ src/components/CareerObjective.jsx
@@ -0,0 +1,13 @@
+import { tw } from "twind";
+
+const CareerObjective = () => (
+  <div>
+    <h1 className={tw`uppercase text-4xl font-bold mb-2`}>career objective</h1>
+    <p>
+      Looking for a challenging software engineer position where I can use my
+      skills and knowledge for software program and learning new technologies.
+    </p>
+  </div>
+);
+
+export default CareerObjective;
